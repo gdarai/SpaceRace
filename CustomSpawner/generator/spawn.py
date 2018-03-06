3bfd56fea52805f21e4bde4213319963c847d3be
Martin Holecek <martin.holecek@deutsche-boerse.com>
2018-03-06 16:03:37 +0100

    Functioning CustomSpawner ScriptGenerator

--- CustomSpawner/generator/spawn.py
+++ CustomSpawner/generator/spawn.py
@@ -1,11 +1,15 @@
 import re
 import os
 import copy
+import shutil
 
 ########
 # Globals
 canPrint = True;
 
+typeParams = ['D:chunkgenspawnchance', 'I:mobspawnrange', 'S:shouldseesky', 'I:spawncap', 'I:spawntickrate', 'I:maxspawnheight', 'I:minspawnheight']
+entitParams = ['B:canSpawn', 'I:frequency', 'I:maxChunk', 'I:maxLightlevel', 'I:maxSpawn', 'I:maxSpawnHeight', 'I:minLightlevel', 'I:minSpawn', 'I:minSpawnHeight', 'S:opaqueBlock', 'S:spawnBlockBlacklist ']
+# + 'S:biomegroups ' + 'S:type' #
 ########
 # Superglobals
 
@@ -17,13 +21,13 @@ def printWarning(text):
 	print('WARN '+text);
 
 def writeLine(f, intention, text):
-	f.write('\t'*intention+text+'\n');
+	f.write('    '*intention+text+'\n');
 
 def writeLineCondition(f, intention, textNo, textYes, condition):
 	if(condition):
-		f.write('\t'*intention+textYes+'\n');
+		f.write('    '*intention+textYes+'\n');
 	else:
-		f.write('\t'*intention+textNo+'\n');
+		f.write('    '*intention+textNo+'\n');
 
 def	writeLineBlocks(f,intention, texts):
 	allBut = texts[0:-1]
@@ -84,6 +88,25 @@ def processLastObject( data, newObject ):
         data['types'][newObject['name']]=copy.deepcopy(newObject)
         return data
 
+def readList( pointyList ):
+    return pointyList[1:-1].split(":")
+
+def readParam( param ):
+    if("=" in param ):
+        return param[4:].split("=")
+    elif("<" in param ):
+        spl = param[4:].split("<")
+        name = spl[0]
+        lst = spl[1][0:-1].split(":")
+        out = [name] + lst
+        return out
+    else:
+        printError('cannot process parameter '+param)
+
+def addParameter( parDict, newPar ):
+    parName = readParam(newPar)[0]
+    parDict[parName] = newPar
+    return parDict
 ########
 # Data loading
 
@@ -92,21 +115,162 @@ print('\n--> Parsing Groups')
 lines = getLinesInFiles('source.*\.txt')
 
 data = {}
-data['types'] = {}
 data['biomes'] = {}
+data['owners'] = {}
+data['types'] = {}
 newObject={}
 for line in lines:
     if(line[0:2]=='//'):
         continue
     if((line == '\n')or(line == '\r\n')):
         continue
-    if(line[0:4]=='TYPE'):
+    cmd = parseLine(line)[0]
+    if(cmd=='TYPE'):
         data = processLastObject(data, newObject)
         ln = parseLine(line)
         newObject = {}
         newObject['type'] = 'type'
         newObject['name'] = ln[1]
-        newObject['params'] = []
-    if(line[0:4]=='BIOME'):
+        newObject['params'] = {}
+        newObject['entities'] = []
+        newObject['owner'] = ''
+        print('NEW Type Group '+ln[1])
+    elif(cmd=='BIOME'):
+        data = processLastObject(data, newObject)
+        newObject = {}
         ln = parseLine(line)
-        data['biomes'][ln[1]] = ln[2]
+        data['biomes'][ln[1]] = readList(ln[2])
+        print('NEW Biome Group '+ln[1]+' '+str(len(data['biomes'][ln[1]])))
+    elif(newObject['type']=='type'):
+        if(cmd=='CREATURES'):
+            newObject['entities'] = parseLine(line)[1:]
+            print(' - ENTRIES:'+str(newObject['entities']))
+        elif(cmd=='PARENT'):
+            parName = parseLine(line)[1]
+            if(parName in data['types']):
+                print(' - PARENT: '+parName)
+                sourcePar =  data['types'][parName]['params']
+                for par in sourcePar.keys():
+                    newObject['params'] = addParameter(newObject['params'], sourcePar[par]);
+            else:
+                printError('parent '+parName+' not existent ('+str(data['types'].keys())+')')
+        elif(cmd=='OWNER'):
+            owner = parseLine(line)[1]
+            newObject['owner'] = owner
+            print(' - OWNER: '+owner)
+            if owner in data['owners']:
+                data['owners'][owner].append(newObject['name'])
+            else:
+                data['owners'][owner] = [newObject['name']]
+        else:
+            newObject['params'] = addParameter(newObject['params'], line[0:-1]);
+            print(line[0:-1])
+data = processLastObject(data, newObject)
+
+# Printing
+print('\nCan Print: '+str(canPrint))
+if canPrint == False:
+	exit()
+
+# Empty the output directory
+directory = 'output'
+if os.path.exists(directory):
+    shutil.rmtree(directory)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Let's do it
+data['usedTypes'] = {}
+data['usedBiomes'] = {}
+crDir = directory+'/'+'Creatures'
+
+os.makedirs(crDir)
+# Print Creatures files
+for owner in data['owners'].keys():
+    f = open(crDir+'/'+owner+'.cfg','w')
+    print('Printing '+crDir+'/'+owner+'.cfg')
+    writeLine(f,0,'# Configuration file')
+    for entityType in data['owners'][owner]:
+        print(' - Entity '+entityType)
+        data['usedTypes'][entityType] = 'used'
+        d = data['types'][entityType]
+        for anim in d['entities']:
+            print(' - - '+anim)
+            writeLine(f,0,'')
+            writeLine(f,0,'####################')
+            writeLine(f,0,'# '+anim)
+            writeLine(f,0,'####################')
+            writeLine(f,0,'')
+            writeLine(f,0,anim+' {')
+            if 'S:biomegroups ' not in d['params']:
+                printError('entity '+entityType+' is missing parameter S:biomegroups ')
+            biom = readParam(d['params']['S:biomegroups '])
+            biomName = owner+'_'+anim.upper()+'_DEFAULT'
+            data['biomes'][biomName] = biom[1:]
+            writeLine(f,1,'S:biomegroups <'+biomName+'>')
+            for param in entitParams:
+                if param not in d['params']:
+                    printError('entity '+entityType+' is missing parameter '+param)
+                else:
+                    writeLine(f,0,d['params'][param])
+            biom = readParam(d['params']['S:biomegroups '])
+            for b in biom[1:]:
+                data['usedBiomes'][biomName] = 'used'
+            writeLine(f,1,'S:type='+entityType)
+            writeLine(f,0,'}')
+    f.close()
+# Print EntitySpawnTypes file
+f = open(directory+'/EntitySpawnTypes.cfg','w')
+print('Printing '+directory+'/EntitySpawnTypes.cfg')
+writeLine(f,0,'# Configuration file')
+for typ in data['usedTypes'].keys():
+    print(' - typ '+typ)
+    writeLine(f,0,'')
+    d = data['types'][typ]
+    writeLine(f,0,'####################')
+    writeLine(f,0,'# '+typ)
+    writeLine(f,0,'####################')
+    writeLine(f,0,'')
+    writeLine(f,0,typ+' {')
+    for param in typeParams:
+        if param not in d['params']:
+            printError('entity '+typ+' is missing parameter '+param)
+        else:
+            writeLine(f,0,d['params'][param])
+    writeLine(f,0,'}')
+f.close()
+# Print EntityBiomeGroups file
+f = open(directory+'/EntityBiomeGroups.cfg','w')
+print('Printing '+directory+'/EntityBiomeGroups.cfg')
+writeLine(f,0,'# Configuration file')
+writeLine(f,0,'')
+writeLine(f,0,'####################')
+writeLine(f,0,'# biomegroup-defaults')
+writeLine(f,0,'####################')
+writeLine(f,0,'')
+writeLine(f,0,'biomegroup-defaults {')
+for biom in data['usedBiomes'].keys():
+    print(' - biome '+biom)
+    biomOut = {}
+    biomOut[data['biomes'][biom][0]]='ok'
+    parseBiom = True
+    while parseBiom:
+        parseBiom = False
+        biomIn = biomOut.keys()
+        biomOut = {}
+        for nm in biomIn:
+            if(nm==""):
+                continue
+            if "|" in nm:
+                biomOut[nm] = 'ok'
+            else:
+                if nm not in data['biomes']:
+                    printError('when interpretting biome '+biom+' found missing definition: '+nm)
+                for b in data['biomes'][nm]:
+                    biomOut[b] = 'ok'
+                parseBiom = True
+    biomeListString = ":".join(biomOut.keys())
+    writeLine(f,1,biom+' <'+biomeListString+'>')
+writeLine(f,0,'}')
+writeLine(f,0,'')
+f.close()
